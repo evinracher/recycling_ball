7226c05e62ce17b19df92ebd48ab66f725e24958
evinracher <eevinley@gmail.com>
2021-05-01 16:56:23 -0500

    adding final code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,177 +5,207 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Garbage {
+public class Garbage
+{
   private string colorCode;
   private string description;
 
-  public Garbage(string colorCode, string description){
+  public Garbage(string colorCode, string description)
+  {
     this.colorCode = colorCode;
     this.description = description;
   }
 
-  public string getColorCode() {
+  public string getColorCode()
+  {
     return this.colorCode;
   }
 
-  public string getDescription() {
+  public string getDescription()
+  {
     return this.description;
   }
 
-  public override string ToString(){
+  public override string ToString()
+  {
     return "{ colorCode: " + this.colorCode + ", description: " + this.description + " }";
   }
 }
 
 public class PlayerController : MonoBehaviour
 {
-    // Variable para el rigidbody del componente
-    private Rigidbody rb;
-    // Variable para la meta de puntos a alcanzar
-    private int goal = 12;
-    // Variable estática para contador
-    // Esta variable pertenece a la clase PlayerController y es accesible por las demás clases
-    public static int count;
-    public static bool charged;
+  // Variable para el rigidbody del componente
+  private Rigidbody rb;
+  // Variable para la meta de puntos a alcanzar
+  public static int goal;
+  // Puntos para abrir el portal
+  public static int toOpen;
+  // Esta variable pertenece a la clase PlayerController y es accesible por las demás clases
+  public int count;
+  public bool charged;
+  private Garbage myGarbage;
 
-    private Garbage[] garbageArray = {
-      new Garbage("verde", "cáscaras de frutas"),
-      new Garbage("negro", "papel higiénico"),
-      new Garbage("negro", "tapabocas"),
-      new Garbage("negro", "guantes"),
-      new Garbage("blanco", "plástico"),
-      new Garbage("blanco", "envase plástico de refresco"),
-      new Garbage("blanco", "caja de cartón"),
-      new Garbage("blanco", "envase de vidrio"),
-      new Garbage("blanco", "pending"),
-      new Garbage("negro", "pending"),
-      new Garbage("negro", "pending"),
-      new Garbage("negro", "pending"),
-      new Garbage("negro", "pending"),
-      new Garbage("negro", "pending"),
+  public const string GREEN = "verde";
+  public const string BLACK = "negro";
+  public const string WHITE = "blanco";
+
+  private Garbage[] garbageArray = {
+      new Garbage(GREEN, "cáscaras de frutas"),
+      new Garbage(WHITE, "plástico"),
+      new Garbage(GREEN, "hojas"),
+      new Garbage(BLACK, "papel higiénico"),
+      new Garbage(GREEN, "residuos de jardín"),
+      new Garbage(WHITE, "envase plástico de refresco"),
+      new Garbage(BLACK, "tapabocas"),
+      new Garbage(WHITE, "caja de cartón"),
+      new Garbage(BLACK, "guantes"),
+      new Garbage(GREEN, "telas naturales"),
+      new Garbage(WHITE, "envase de vidrio"),
+      new Garbage(BLACK, "pañales"),
+      new Garbage(WHITE, "periódicos"),
+      new Garbage(BLACK, "arena para gato"),
     };
 
-    // Variable pública para definir la velocidad del jugador
-    public float speed = 1;
-    // Variables públicas para texto
-    public TextMeshProUGUI countText;
-    public TextMeshProUGUI portalText;
-    public TextMeshProUGUI infoText;
-    public GameObject portalIndicator;
-    public GameObject loseText;
-    public GameObject winText;
+  // Variable pública para definir la velocidad del jugador
+  public float speed = 1;
+  // Variables públicas para texto
+  public TextMeshProUGUI countText;
+  public TextMeshProUGUI portalText;
+  public TextMeshProUGUI infoText;
+  public GameObject portalIndicator;
+  public GameObject loseText;
+  public GameObject winText;
 
-    // Se ejecuta una vez, al iniciar el programa
-    void Start()
+  // Se ejecuta una vez, al iniciar el programa
+  void Start()
+  {
+    count = 0;
+    rb = GetComponent<Rigidbody>();
+    // foreach (var item in garbageArray)
+    // {
+    //   Debug.Log(item.ToString());
+    // }
+    // inicializar objetos para textos
+    winText.SetActive(false);
+    loseText.SetActive(false);
+    infoText.gameObject.SetActive(false);
+    portalIndicator.SetActive(false);
+    portalText.gameObject.SetActive(false);
+
+    // Contar la cantidad de pickups en el mapa
+    if (goal <= 0)
     {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-        foreach(var item in garbageArray)
-        {
-          Debug.Log(item.ToString());
-        }
-        // inicializar objetos para textos
-        winText.SetActive(false);
-        loseText.SetActive(false);
+      goal = GameObject.FindGameObjectsWithTag("PickUp").Length;
+    }
+    toOpen = goal - 2;
+    countText.text = "Count: " + count.ToString();
+  }
+
+  // Se ejecuta en cada frame
+  void Update()
+  {
+    // obtener posición en el eje y del jugador
+    float yPosition = transform.position.y;
+
+    // Verificar si caimos al abismo, imprimir mensaje e ir a la escena de Game Over
+    if (yPosition < 0.0 && count != goal)
+    {
+      loseText.SetActive(true);
+      StartCoroutine(GameOver());
+    }
+  }
+
+  // Timer para cambio de escena a GameOver
+  IEnumerator GameOver()
+  {
+    yield return new WaitForSecondsRealtime(2);
+    SceneManager.LoadScene("GameOver");
+  }
+
+  // Timer para cambio de escena a MainMenu
+  IEnumerator Win()
+  {
+    yield return new WaitForSecondsRealtime(2);
+    SceneManager.LoadScene("MainMenu");
+  }
+
+  // Timer para imprimir el texto de ayuda para el portal
+  IEnumerator PortalPrint()
+  {
+    portalText.gameObject.SetActive(true);
+    yield return new WaitForSecondsRealtime(3);
+    portalText.gameObject.SetActive(false);
+  }
+
+  // Imprimir texto en la pantalla
+  void SetText()
+  {
+    countText.text = "Count: " + count.ToString();
+    if (count >= goal)
+    {
+      winText.SetActive(true);
+
+      // Ir a la escena de inicio
+      StartCoroutine(Win());
+    }
+
+    // Mostrar mensaje de ir al portal
+    if (count == toOpen && !charged)
+    {
+      portalText.text = "Go to the portal";
+      portalIndicator.SetActive(true);
+      StartCoroutine(PortalPrint());
+    }
+  }
+
+  // Se ejecuta en cada frame
+  private void FixedUpdate()
+  {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    rb.AddForce(movement * speed);
+  }
+
+  // Cuando entramos en colisión con un trigger collider
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("Bin") && charged)
+    {
+      string binColor = other.gameObject.GetComponent<binController>().color;
+      if (binColor.Equals(myGarbage.getColorCode()))
+      {
+        count = count + 1;
+        infoText.text = "";
         infoText.gameObject.SetActive(false);
-        portalIndicator.SetActive(false);
-        portalText.gameObject.SetActive(false);
-
-        // Contar la cantidad de pickups en el mapa
-        goal = GameObject.FindGameObjectsWithTag("PickUp").Length;
-        countText.text = "Count: " + count.ToString();
+        charged = false;
+      }
+      else
+      {
+        infoText.text = "Residuo: " + myGarbage.getDescription() + "\nCaneca equivocada";
+      }
+      SetText();
     }
 
-    // Se ejecuta en cada frame
-    void Update()
+    if (other.gameObject.CompareTag("PickUp") && !charged)
     {
-        // obtener posición en el eje y del jugador
-        float yPosition = transform.position.y;
+      charged = true;
+      other.gameObject.SetActive(false);
 
-        // Verificar si caimos al abismo, imprimir mensaje e ir a la escena de Game Over
-        if (yPosition < 0.0)
-        {
-            loseText.SetActive(true);
-            StartCoroutine(GameOver());
-        }
+      infoText.text = "Residuo: " + garbageArray[count].getDescription();
+      infoText.gameObject.SetActive(true);
+      myGarbage = garbageArray[count];
     }
+  }
 
-    // Timer para cambio de escena a GameOver
-    IEnumerator GameOver()
+  // Destruir paredes al tocarlas
+  void OnCollisionEnter(Collision other)
+  {
+    // Desactivar pared si hubo colisión
+    if (other.gameObject.CompareTag("Wall"))
     {
-        yield return new WaitForSecondsRealtime(2);
-        SceneManager.LoadScene("GameOver");
+      other.gameObject.SetActive(false);
     }
-
-    // Timer para cambio de escena a MainMenu
-    IEnumerator Win()
-    {
-        yield return new WaitForSecondsRealtime(2);
-        SceneManager.LoadScene("MainMenu");
-    }
-
-    // Timer para imprimir el texto de ayuda para el portal
-    IEnumerator PortalPrint()
-    {
-        portalText.gameObject.SetActive(true);
-        yield return new WaitForSecondsRealtime(3);
-        portalText.gameObject.SetActive(false);
-    }
-    
-    // Imprimir texto en la pantalla
-    void SetText()
-    {
-        countText.text = "Count: " + count.ToString();
-        if (count >= goal)
-        {
-            winText.SetActive(true);
-
-            // Ir a la escena de inicio
-            StartCoroutine(Win());
-        }
-
-        // Mostrar mensaje de ir al portal
-        if (count == 12)
-        {
-            portalText.text = "Go to the portal";
-            portalIndicator.SetActive(true);
-            StartCoroutine(PortalPrint());
-        }
-    }
-
-    // Se ejecuta en cada frame
-    private void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
-    }
-
-    // Cuando entramos en colisión con un trigger collider
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("PickUp"))
-        {
-            
-            other.gameObject.SetActive(false);
-            
-            infoText.text = "Residuo: " + garbageArray[count].getDescription();
-            infoText.gameObject.SetActive(true);
-
-            count = count + 1;
-            SetText();
-        }
-    }
-
-    // Destruir paredes al tocarlas
-    void OnCollisionEnter(Collision other)
-    {
-        // Desactivar pared si hubo colisión
-        if (other.gameObject.CompareTag("Wall"))
-        {
-            other.gameObject.SetActive(false);
-        }
-    }
+  }
 }
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,40 +5,41 @@ using TMPro;
 
 public class Portal : MonoBehaviour
 {
-    // Una variable pública que guarda nuestra posición de salida
-    public Transform exit;
-    public TextMeshProUGUI portalText;
-    public GameObject portalIndicator;
+  // Una variable pública que guarda nuestra posición de salida
+  public Transform exit;
+  public TextMeshProUGUI portalText;
+  public GameObject portalIndicator;
 
-    // Al intersectarse con el portal
-    private void OnTriggerEnter(Collider other)
+  // Al intersectarse con el portal
+  private void OnTriggerEnter(Collider other)
+  {
+    // Comprobar si tenemos más de doce puntos
+    if (other.gameObject.CompareTag("Player") 
+    && other.gameObject.GetComponent<PlayerController>().count >= PlayerController.toOpen)
     {
-        // Comprobar si tenemos más de doce puntos
-        if (PlayerController.count >= 12)
-        {
-            // Mover al objeto a la posición de salida
-            other.gameObject.transform.position = exit.position;
-            // Poniendo la velocidad del jugador en cero
-            Rigidbody rb = other.GetComponent<Rigidbody>();
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-        }
-        // Sino, poner mensaje para indicarlo
-        else
-        {
-            portalText.text = "You need 12 points to use the portal";
-            StartCoroutine(PortalPrint());
-        }
-
+      // Mover al objeto a la posición de salida
+      other.gameObject.transform.position = exit.position;
+      // Poniendo la velocidad del jugador en cero
+      Rigidbody rb = other.GetComponent<Rigidbody>();
+      rb.velocity = Vector3.zero;
+      rb.angularVelocity = Vector3.zero;
+    }
+    // Sino, poner mensaje para indicarlo
+    else
+    {
+      portalText.text = "You need 12 points to use the portal";
+      StartCoroutine(PortalPrint());
     }
 
-    // Código para timer, esperar ciertos segundos y luego desactivar el texto y el indicador
-    IEnumerator PortalPrint()
-    {
-        portalIndicator.SetActive(true);
-        portalText.gameObject.SetActive(true);
-        yield return new WaitForSecondsRealtime(3);
-        portalIndicator.SetActive(false);
-        portalText.gameObject.SetActive(false);
-    }
+  }
+
+  // Código para timer, esperar ciertos segundos y luego desactivar el texto y el indicador
+  IEnumerator PortalPrint()
+  {
+    portalIndicator.SetActive(true);
+    portalText.gameObject.SetActive(true);
+    yield return new WaitForSecondsRealtime(3);
+    portalIndicator.SetActive(false);
+    portalText.gameObject.SetActive(false);
+  }
 }
